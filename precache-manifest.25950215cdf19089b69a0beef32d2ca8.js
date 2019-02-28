063d97be79454111340890ee5269fb830814e766
django18 <avinashdangi.26@gmail.com>
2019-03-01 01:13:07 +0530

    Updates

---
+++ precache-manifest.25950215cdf19089b69a0beef32d2ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c295a830f76793d5f35",
+    "url": "/Portfolio/static/css/main.1a067840.chunk.css"
+  },
+  {
+    "revision": "2c295a830f76793d5f35",
+    "url": "/Portfolio/static/js/main.2c295a83.chunk.js"
+  },
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/Portfolio/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bc5cf749be000a8c746fe720b2598eab",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a6c1b2651630d3bd8f41e469c05227e.js"
+  "/Portfolio/precache-manifest.25950215cdf19089b69a0beef32d2ca8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
